1614e3d6846ddd26eba99d322d5237ffb0d6a638
yukesh dhakal <102406963+YukeshDhakal@users.noreply.github.com>
2026-06-07 14:48:33 +1000

    Add files via upload

---
+++ RestockForm.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace SmartRepositorySystem
+{
+    partial class RestockForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.NumericUpDown numQuantity;
+        private System.Windows.Forms.Button btnConfirm;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label lblItemInfo;
+        private System.Windows.Forms.Label lblPrompt;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.numQuantity = new System.Windows.Forms.NumericUpDown();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lblItemInfo = new System.Windows.Forms.Label();
+            this.lblPrompt = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numQuantity)).BeginInit();
+            this.SuspendLayout();
+
+            this.lblItemInfo.Location = new System.Drawing.Point(20, 20);
+            this.lblItemInfo.Size = new System.Drawing.Size(300, 30);
+
+            this.lblPrompt.Text = "Quantity to add:";
+            this.lblPrompt.Location = new System.Drawing.Point(20, 60);
+            this.lblPrompt.Size = new System.Drawing.Size(120, 25);
+
+            this.numQuantity.Location = new System.Drawing.Point(150, 60);
+            this.numQuantity.Size = new System.Drawing.Size(100, 27);
+            this.numQuantity.Minimum = 1;
+            this.numQuantity.Maximum = 10000;
+
+            this.btnConfirm.Text = "Confirm";
+            this.btnConfirm.Location = new System.Drawing.Point(80, 110);
+            this.btnConfirm.Size = new System.Drawing.Size(100, 35);
+            this.btnConfirm.BackColor = System.Drawing.Color.LightGreen;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
+
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.Location = new System.Drawing.Point(200, 110);
+            this.btnCancel.Size = new System.Drawing.Size(100, 35);
+            this.btnCancel.BackColor = System.Drawing.Color.LightCoral;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+
+            this.Text = "Restock Item";
+            this.Size = new System.Drawing.Size(350, 180);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+
+            this.Controls.Add(this.lblItemInfo);
+            this.Controls.Add(this.lblPrompt);
+            this.Controls.Add(this.numQuantity);
+            this.Controls.Add(this.btnConfirm);
+            this.Controls.Add(this.btnCancel);
+
+            ((System.ComponentModel.ISupportInitialize)(this.numQuantity)).EndInit();
+            this.ResumeLayout(false);
+        }
+    }
+}
---
+++ RestockForm.cs
@@ -0,0 +1,35 @@
+﻿using SmartInventorySystem;
+using System;
+using System.Windows.Forms;
+
+namespace SmartRepositorySystem
+{
+    public partial class RestockForm : Form
+    {
+        public int QuantityToAdd { get; private set; }
+        private PerishableItem _item;
+
+        public RestockForm(PerishableItem item)
+        {
+            InitializeComponent();
+            _item = item;
+        }
+
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+            QuantityToAdd = (int)numQuantity.Value;
+            if (QuantityToAdd <= 0)
+            {
+                MessageBox.Show("Please enter a quantity greater than 0.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            this.Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            QuantityToAdd = 0;
+            this.Close();
+        }
+    }
+}
